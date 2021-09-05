f7c0e74b4da4545213d368ff33e0964a3e56d6b5
David Lemvigh <dlemvigh@gmail.com>
2021-09-05 20:46:51 +0200

    Handle server start and close in calc-api

--- calc-api/index.ts
+++ calc-api/index.ts
@@ -1,3 +1,29 @@
 import express from "express";
 
-console.log("foo", express);
+const app = express();
+
+const PORT = process.env.PORT || 8081;
+
+app.get("/health", (req, res) => {
+  res.status(200);
+  res.send("Calculation API is health");
+});
+
+app.get("/factorial", async (req, res) => {});
+
+app.post("/factorial", async (req, res) => {});
+
+app.put("/factorial", async (req, res) => {});
+
+const server = app.listen(PORT, () => {
+  console.log(`server listening to port ${PORT}`);
+});
+
+async function close(signal: NodeJS.Signals) {
+  console.log(`Received signal to close ${signal}`);
+  await server.close();
+  process.exit();
+}
+
+process.on("SIGINT", close);
+process.on("SIGTERM", close);
