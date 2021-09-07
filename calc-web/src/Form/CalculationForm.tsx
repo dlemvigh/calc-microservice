e44d19d8f857e8fd85192b8dc189b3d4cfe57919
David Lemvigh <dlemvigh@gmail.com>
2021-09-07 21:17:07 +0200

    Fix lint errors

--- calc-web/src/Form/CalculationForm.tsx
+++ calc-web/src/Form/CalculationForm.tsx
@@ -4,7 +4,8 @@ import { Button } from "@material-ui/core";
 import { CalculationInput } from "./CalculationInput";
 import * as Yup from "yup";
 import { Row } from "../Components/Flex";
-import { postFactorial, useCreateFactorial } from "../Query/factorial";
+import { useCreateFactorial } from "../Query/factorial";
+
 const validationSchema = Yup.object().shape({
     input: Yup.number()
         .min(0, "Must be a positive number")
@@ -18,11 +19,12 @@ interface FormValues {
 
 export function CalculationForm() {
     const mutate = useCreateFactorial();
+
     const handleSubmit = useCallback(async (values: FormValues, helpers: FormikHelpers<FormValues>) => {
         await mutate.mutateAsync({ input: values.input });
         // await postFactorial({ input: values.input });
         helpers.resetForm();
-    }, []);
+    }, [mutate]);
 
     return (
         <Formik
