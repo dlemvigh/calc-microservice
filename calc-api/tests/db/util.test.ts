05bf0e4b8bcd0c7cfcf0a6dd981d14c105f1b95c
David Lemvigh <dlemvigh@gmail.com>
2021-09-26 11:11:40 +0200

    Unit test missing output for non-trunkated output

---
+++ calc-api/tests/db/util.test.ts
@@ -0,0 +1,19 @@
+import "mocha";
+import { expect } from "chai";
+import { trunkOutput } from "../../src/routes/factorial/util";
+
+describe.only("trunk output - max length 3", () => {
+  const cases: [string, string][] = [
+    ["1", "1"],
+    ["12", "12"],
+    ["123", "123"],
+    ["1234", "123e1"],
+    ["12345", "123e2"],
+    ["123456", "123e3"],
+  ];
+  cases.forEach(([input, expected]) => {
+    it(`${input} => ${expected}`, () => {
+      expect(trunkOutput(input, 3)).to.equal(expected);
+    });
+  });
+});
