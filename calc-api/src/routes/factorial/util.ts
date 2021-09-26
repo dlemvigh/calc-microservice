0284a43ef871056caccc2f809429609f92dfea00
David Lemvigh <dlemvigh@gmail.com>
2021-09-26 11:11:52 +0200

    Fix missing trunkated output bug

--- calc-api/src/routes/factorial/util.ts
+++ calc-api/src/routes/factorial/util.ts
@@ -2,4 +2,5 @@ export function trunkOutput(output: string | undefined, maxLength: number) {
   if (output && output.length > maxLength) {
     return output.substr(0, maxLength) + "e" + (output.length - maxLength);
   }
+  return output;
 }
