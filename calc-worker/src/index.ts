c0da3ca2df7ff139a5b8fc06b29d6eaa4c151e88
David Lemvigh <dlemvigh@gmail.com>
2021-09-27 08:50:21 +0200

    Move work loop from index.ts to worker.ts

--- calc-worker/src/index.ts
+++ calc-worker/src/index.ts
@@ -1,50 +1,11 @@
 import { sqsClient } from "./aws/sqs";
 import { postResult } from "./api/api";
-import { factorial } from "./api/calc";
+import { worker } from "./worker";
+
 import { config } from "./config";
 
-let working = false;
 const sqs = sqsClient(config);
-
-function log(message: any, ...other: any[]) {
-  if (config.DEBUG) {
-    print(message, ...other);
-  }
-}
-function error(message: any, ...other: any[]) {
-  print(message, ...other);
-}
-
-function print(message: any, ...other: any[]) {
-  console.log(new Date().toLocaleTimeString(), " | ", message, ...other);
-}
-
-async function loop() {
-  if (working) {
-    console.log("working...");
-    return;
-  }
-  try {
-    working = true;
-    const msg = await sqs.receiveMessage();
-    log("received message from queue");
-    await postResult(config)({ ...msg.json, calcStartedAt: new Date() });
-    log("started calculation");
-    const result = await factorial(msg.json.input);
-    log("finished calculation", result.toString().length);
-    await postResult(config)({
-      ...msg.json,
-      finishedAt: new Date(),
-      output: result.toString(),
-    });
-    log("posted result to api");
-    await sqs.deleteMessage(msg);
-    log("deleted message from queue");
-  } catch (err) {
-    error("err", err);
-  }
-  working = false;
-}
+const loop = worker(config, sqs, postResult);
 
 const interval = setInterval(loop, config.POLLING_INTERVAL);
 
@@ -53,6 +14,5 @@ async function close(signal: NodeJS.Signals) {
   clearInterval(interval);
   process.exit();
 }
-
 process.on("SIGINT", close);
 process.on("SIGTERM", close);
