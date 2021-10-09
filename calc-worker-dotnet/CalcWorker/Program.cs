1fde2fa8a69380745939e6b9a5b11325b844f7c7
David Lemvigh <dle@siteimprove.com>
2021-10-09 20:55:15 +0200

    Add empty console and test project
    With proper names and references,
    but without meaning ful code

---
+++ calc-worker-dotnet/CalcWorker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalcWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Foo");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
