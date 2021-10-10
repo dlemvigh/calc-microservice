df26f77b058904382bcc2d627881b871dc1918ea
David Lemvigh <dlemvigh@gmail.com>
2021-10-11 01:42:16 +0200

    Worker

--- calc-worker-dotnet/CalcWorker/Work/Worker.cs
+++ calc-worker-dotnet/CalcWorker/Work/Worker.cs
@@ -1,5 +1,57 @@
+using System;
+using System.Threading.Tasks;
+using CalcWorker.Api;
+using CalcWorker.Config;
+using CalcWorker.Queue;
+using Microsoft.Extensions.Logging;
+
 namespace CalcWorker.Work {
     public interface IWorker {
-
+        Task DoWork();
     }
+
+    public class Worker : IWorker {
+        private readonly ILogger<Worker> logger;
+        private readonly IEnvConfig config;
+        private readonly IQueueClient queueClient;
+        private readonly IApiClient apiClient;
+        private readonly ICalculator calculator;
+
+        public Worker(
+            ILoggerFactory loggerFactory, 
+            IEnvConfig config, 
+            IQueueClient queueClient, 
+            IApiClient apiClient, 
+            ICalculator calculator
+        ) {
+            this.logger = loggerFactory.CreateLogger<Worker>();
+            this.config = config;
+            this.queueClient = queueClient;
+            this.apiClient = apiClient;
+            this.calculator = calculator;
+        }
+
+        public async Task DoWork() {
+            logger.LogInformation("Receive message");
+            var message = await queueClient.ReceiveMessageAsync();
+
+            if (message == null) {
+                logger.LogInformation("No messages");
+                return;
+            }
+
+            var job = message.Job;
+            job.CalcStartedAt = new DateTime();
+            logger.LogInformation("Update job - calculation started");
+            await apiClient.PostResultAsync(job);
+
+            job.Output = calculator.Factorial(job.Input);
+            job.FinishedAt = new DateTime();
+            logger.LogInformation("Update job - calculation finished");
+            await apiClient.PostResultAsync(job);
+
+            await queueClient.DeleteMessageAsync(message);
+        }
+
+  }
 }
