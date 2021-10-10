6c55b3c8bf3af041ce8a5f3080f054ecc25e8811
David Lemvigh <dlemvigh@gmail.com>
2021-10-10 22:13:08 +0200

    Inject config

--- calc-worker-dotnet/CalcWorker/Api/ApiClient.cs
+++ calc-worker-dotnet/CalcWorker/Api/ApiClient.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json;
 using System.Text;
+using Microsoft.Extensions.Logging;
+using CalcWorker.Config;
 
 namespace CalcWorker.Api
 {
@@ -13,18 +15,22 @@ namespace CalcWorker.Api
     public class ApiClient : IApiClient
     {
         private readonly IHttpClientFactory httpClientFactory;
-        public ApiClient(IHttpClientFactory httpClientFactory)
+        private readonly ILogger<ApiClient> logger;
+        private readonly IEnvConfig config;
+        public ApiClient(IHttpClientFactory httpClientFactory, ILoggerFactory loggerFactory, IEnvConfig config)
         {
             this.httpClientFactory = httpClientFactory;
+            this.logger = loggerFactory.CreateLogger<ApiClient>();
+            this.config = config;
         }
         public async Task PostResultAsync(JobDTO job)
         {
             using (var httpClient = httpClientFactory.CreateClient())
             {
-                var endpoint = $"http://localhost:8081/factorial/{job.Id}";
+                var endpoint = $"{config.ApiEndpoint}/factorial/{job.Id}";
                 var json = JsonConvert.SerializeObject(job);
                 var data = new StringContent(json, Encoding.UTF8, "application/json");
-
+                logger.LogInformation($"PUT {data}");
                 await httpClient.PutAsync(endpoint, data);
             }
         }
