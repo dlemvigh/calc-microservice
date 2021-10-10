a0f11425b85c27d1a3db6d82a36c2e6a4fe21591
David Lemvigh <dlemvigh@gmail.com>
2021-10-10 21:53:16 +0200

    Add EnvConfig

---
+++ calc-worker-dotnet/CalcWorker/Config/DependencyInjection.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using CalcWorker.Api;
+using CalcWorker.Work;
+using CalcWorker.Queue;
+
+namespace CalcWorker.Config
+{
+    public class DependencyInjection
+    {
+        public static IServiceProvider ConfigureServices()
+        {
+
+            var services = new ServiceCollection();
+
+            // Configure your services here
+            services.AddSingleton<IApiClient, ApiClient>();
+            services.AddSingleton<ICalculator, Calculator>();
+            services.AddSingleton<IQueueClient, QueueClient>();
+            services.AddSingleton<IEnvConfig, EnvConfig>();
+            services.AddLogging(logConfig =>
+                logConfig.AddSimpleConsole(formatConfig =>
+                {
+                    formatConfig.SingleLine = true;
+                    formatConfig.TimestampFormat = "[hh:mm:ss] ";
+                })
+            );
+            services.AddHttpClient();
+
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider;
+        }
+    }
+}
