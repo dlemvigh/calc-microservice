0593f09b4d588fece8ea7d78b916f6563c614f7d
David Lemvigh <dle@siteimprove.com>
2021-10-11 19:08:20 +0200

    QueueClient tests

---
+++ calc-worker-dotnet/CalcWorker.Test/Queue/QueueClientTests.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using CalcWorker.Config;
+using CalcWorker.Queue;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using NUnit.Framework;
+
+namespace CalcWorker.Test.Queue {
+	public class QueueClientTests {
+		[Test]
+		public async Task ReceiveMessageAsyncTest_NoMessages_ShouldReturnNull() {
+			// arrange
+			var sqsClient = new Mock<IAmazonSQS>();
+			var loggerFactory = new NullLoggerFactory();
+			var config = new EnvConfig {
+				QueueEndpoint = "http://some-queue.io/my/queue",
+			};
+
+			sqsClient
+				.Setup<Task<ReceiveMessageResponse>>(x => 
+					x.ReceiveMessageAsync(
+						It.Is<ReceiveMessageRequest>(req => req.QueueUrl == config.QueueEndpoint), 
+						It.IsAny<CancellationToken>()))
+				.ReturnsAsync(new ReceiveMessageResponse{
+					Messages = new List<Message>() {}
+				})
+				.Verifiable();
+
+			// act
+			var client = new QueueClient(sqsClient.Object, loggerFactory, config);
+			var message = await client.ReceiveMessageAsync();
+
+			// assert			
+			Assert.IsNull(message);
+			sqsClient.Verify();
+		}
+
+		[Test]
+		public async Task ReceiveMessageAsyncTest_WithMessages_ShouldReturnJobDTO() {
+			// arrange
+			var messageBody = "{ id: 42, input: 5 }";
+
+			var sqsClient = new Mock<IAmazonSQS>();
+			var loggerFactory = new NullLoggerFactory();
+			var config = new EnvConfig {
+				QueueEndpoint = "http://some-queue.io/my/queue",
+			};
+
+			sqsClient
+				.Setup<Task<ReceiveMessageResponse>>(x => 
+					x.ReceiveMessageAsync(
+						It.Is<ReceiveMessageRequest>(req => req.QueueUrl == config.QueueEndpoint), 
+						It.IsAny<CancellationToken>()))
+				.ReturnsAsync(new ReceiveMessageResponse{
+					Messages = new List<Message>() {
+						new Message {
+							Body = messageBody
+						}
+					}
+				})
+				.Verifiable();
+
+			// act
+			var client = new QueueClient(sqsClient.Object, loggerFactory, config);
+			var message = await client.ReceiveMessageAsync();
+
+			// assert			
+			Assert.NotNull(message);
+			Assert.AreEqual(messageBody, message.Body);
+			Assert.AreEqual(42, message.Job.Id);
+			Assert.AreEqual(5, message.Job.Input);
+			Assert.AreEqual(null, message.Job.Output);
+			Assert.AreEqual(null, message.Job.CalcStartedAt);
+			Assert.AreEqual(null, message.Job.FinishedAt);
+			sqsClient.Verify();
+		}
+
+	}
+}
